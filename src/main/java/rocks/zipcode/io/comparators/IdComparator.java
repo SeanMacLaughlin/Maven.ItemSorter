d88dcf1f56feb5f2829dd2142a8e331817f5123c
Sean MacLaughlin <seanmaclaughlin@zipcoders-MacBook-Pro.local>
2019-02-28 17:38:06 -0500

    Item Sorter Completed

--- src/main/java/rocks/zipcode/io/comparators/IdComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/IdComparator.java
@@ -1,7 +1,22 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.LinkedList;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class IdComparator {
+public class IdComparator implements Comparator<Item> {
+    public int compare(Item item1, Item item2) {
+        if (item1.getId() == item2.getId()) {
+            return 0;
+        } else if (item1.getId() > item2.getId()){
+            return 1;
+        } else {
+            return -1;
+        }
+    }
 }
--- src/main/java/rocks/zipcode/io/comparators/NameComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/NameComparator.java
@@ -1,7 +1,20 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class NameComparator {
+public class NameComparator implements Comparator<Item>{
+    public int compare(Item item1, Item item2) {
+        if (item1.getName().equals(item2.getName())) {
+            return 0;
+        } else if (item1.getName().compareTo(item2.getName()) > 0){
+            return 1;
+        } else {
+            return -1;
+        }
+    }
 }
